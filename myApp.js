31a0eaae385ca4b3241c028672bf75cfda75fbcb
sunn-e <sunnydhoke22@gmail.com>
2021-06-14 10:44:30 +0530

    Mount the express.static() middleware to the path /public with app.use(). The absolute path to the assets folder is __dirname + /public.

--- myApp.js
+++ myApp.js
@@ -6,4 +6,6 @@ console.log("Hello World");
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
