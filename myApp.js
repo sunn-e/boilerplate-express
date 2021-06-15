fc62c154c5ccd675e9dcd5128a156045a299c63a
sunn-e <sunnydhoke22@gmail.com>
2021-06-15 08:33:26 +0530

    Serve the object {"message": "Hello json"} as a response, in JSON format, to GET requests to the /json route. Then point your browser to your-app-url/json, you should see the message on the screen.

--- myApp.js
+++ myApp.js
@@ -6,6 +6,9 @@ console.log("Hello World");
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
