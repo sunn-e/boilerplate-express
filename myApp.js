9327716ad75085a788bfe524a6dbd08a1d5af562
sunn-e <sunnydhoke22@gmail.com>
2021-06-15 10:23:52 +0530

    In the route app.get('/now', ...) chain a middleware function and the final handler. In the middleware function you should add the current time to the request object in the req.time key. You can use new Date().toString(). In the handler, respond with a JSON object, taking the structure {time: req.time}.

--- myApp.js
+++ myApp.js
@@ -12,6 +12,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.send({ time: req.time });
+  }
+);
 app.get("/json", (req, res) => {
   let response = "";
   if (process.env.MESSAGE_STYLE === "uppercase") {
